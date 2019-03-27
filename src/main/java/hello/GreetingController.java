bcc202f2f4c76d70c21526f0883056a181f6ac2f
1 <1@1.org>
2019-03-27 15:39:47 +0100

    add web greeting controller

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
+//test
 
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
